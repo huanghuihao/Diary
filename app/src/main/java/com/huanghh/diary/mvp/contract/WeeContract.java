c15e3cf8aed6cf386974e9ed0e4e861a590ca674
huanghuihao <huanghuihao@chually.com>
2018-07-27 13:56:34 +0800

    init diary to github

---
+++ app/src/main/java/com/huanghh/diary/mvp/contract/WeeContract.java
@@ -0,0 +1,12 @@
+package com.huanghh.diary.mvp.contract;
+
+import com.huanghh.diary.mvp.presenter.BasePresenter;
+import com.huanghh.diary.mvp.view.BaseView;
+
+public interface WeeContract {
+    interface View extends BaseView {
+    }
+
+    interface Presenter extends BasePresenter {
+    }
+}
