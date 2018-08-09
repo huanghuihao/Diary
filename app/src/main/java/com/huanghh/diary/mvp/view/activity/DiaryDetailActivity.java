3b332641a9759b88109040ca395f343d727650ca
huanghuihao <398644266@qq.com>
2018-08-09 22:09:07 +0800

    add diary save

--- app/src/main/java/com/huanghh/diary/mvp/view/activity/DiaryDetailActivity.java
+++ app/src/main/java/com/huanghh/diary/mvp/view/activity/DiaryDetailActivity.java
@@ -2,8 +2,10 @@ package com.huanghh.diary.mvp.view.activity;
 
 import com.huanghh.diary.R;
 import com.huanghh.diary.base.BaseActivity;
+import com.huanghh.diary.mvp.contract.DiaryDetailContract;
+import com.huanghh.diary.mvp.presenter.DiaryDetailPresenter;
 
-public class DiaryDetailActivity extends BaseActivity {
+public class DiaryDetailActivity extends BaseActivity<DiaryDetailPresenter> implements DiaryDetailContract.View {
 
     @Override
     protected int setContentLayoutRes() {
